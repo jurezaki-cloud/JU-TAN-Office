feac8b96f4a9e07b86f8d8734e3c8eac71a0ba2c
jurezaki-cloud <jurezaki@gmail.com>
2026-09-24 14:15:54 +0200

    Exercise main window Customers factory without package import cycle

--- tests/test_company_lookup.py
+++ tests/test_company_lookup.py
@@ -3,10 +3,11 @@ import pytest
 from app.modules.customers.services.company_lookup import CompanyLookupError, normalize_tax_number
 
 
-def test_customer_page_is_available_to_main_window():
-    from app.modules.customers import CustomerPage
+def test_customer_page_is_available_to_main_window(qt_app):
+    from app.modules.customers.customer_page import CustomerPage
+    from app.windows.main_window import _page_customers
 
-    assert CustomerPage.__name__ == "CustomerPage"
+    assert isinstance(_page_customers(), CustomerPage)
 
 
 @pytest.mark.parametrize(
